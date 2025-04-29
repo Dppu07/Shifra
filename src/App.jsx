e58cba1c1039b90ec985c220e7c241d565d8e9c8
Darpan Arora <38434164-darpanarora516@users.noreply.replit.com>
2025-04-29 12:20:22 +0000

    Enabled voice Commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css";
 import va from "./assets/ai.png";
 import { CiMicrophoneOn } from "react-icons/ci";
@@ -7,18 +7,55 @@ import speakimg from "./assets/speak.gif";
 import aigif from "./assets/aiVoice.gif";
 
 function App() {
-  let { recognition, startSpeechRecognition, speaking,setSpeaking,prompt,setPrompt,response,setResponse} =
-    useContext(dataContext);
+  let {
+    recognition,
+    startSpeechRecognition,
+    speaking,
+    setSpeaking,
+    prompt,
+    setPrompt,
+    response,
+    setResponse,
+  } = useContext(dataContext);
+
+  // One-time user interaction enabler
+  useEffect(() => {
+    const allowVoice = () => {
+      const test = new SpeechSynthesisUtterance("Voice enabled");
+      window.speechSynthesis.speak(test);
+      document.removeEventListener("click", allowVoice);
+    };
+    document.addEventListener("click", allowVoice);
+  }, []);
+
+  // Function to trigger voice output
+  const speak = (text) => {
+    if (!text || !window.speechSynthesis) return;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = "en-US";
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(utterance);
+  };
+
+  // Speak whenever response is shown
+  useEffect(() => {
+    if (response && prompt) {
+      speak(prompt);
+    }
+  }, [response, prompt]);
+
   return (
     <div className="main">
       <img src={va} alt="logo" id="Shifra" />
       <span>I'm Jarvis Your Advanced Virtual Assistant</span>
-      {!speaking ? 
+
+      {!speaking ? (
         <button
           onClick={() => {
             setPrompt("Listening...");
             setSpeaking(true);
             setResponse(false);
+
             if (recognition) {
               startSpeechRecognition();
             } else {
@@ -28,17 +65,18 @@ function App() {
         >
           Click Here <CiMicrophoneOn />
         </button>
-    : 
+      ) : (
         <div className="response">
-         {!response? 
-         <img src={speakimg} alt="" id="speak" /> 
-           :
-        <img src={aigif} alt="" id="aigif" />
-         }
+          {!response ? (
+            <img src={speakimg} alt="" id="speak" />
+          ) : (
+            <img src={aigif} alt="" id="aigif" />
+          )}
           <p>{prompt}</p>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
+
 export default App;
